9bc31d9033fac23a4401fdcff8f93817a442a758
MikhailKovalenko <kovalenko.mikhail.vadim@gmail.com>
2025-09-09 18:12:42 +0300

    add coverage Withdraw Deposit 1-12

--- tests/withdraw.spec.ts
+++ tests/withdraw.spec.ts
@@ -6,7 +6,7 @@ import * as withdrawPage from "../pages/withdrawPage";
 
 import { beforeEach } from "node:test";
 
-test.describe("Withdraw", () => {
+test.describe("Withdraw Personal Details", () => {
   test.use({
     storageState: getStorageStatePath(undefined, undefined, undefined, "auth"),
   });
@@ -14,6 +14,7 @@ test.describe("Withdraw", () => {
   test.beforeEach(async ({ page }) => {
     await homepage.openHomePage(page);
     await cookies.acceptCookiesIfVisible(page);
+    await withdrawPage.goToPersonalDetails(page);
   });
 
   test("Change Password", async ({ page }) => {
@@ -27,4 +28,56 @@ test.describe("Withdraw", () => {
     await withdrawPage.openModalChangePassword(page);
     await withdrawPage.closeModalChangePassword(page);
   });
+  test("12 Personal Details check elements", async ({ page }) => {
+    await withdrawPage.elementsPersonalDetails(page);
+  })
+
+});
+
+test.describe("Withdraw Deposit", () => {
+  test.use({
+    storageState: getStorageStatePath(undefined, undefined, undefined, "auth"),
+  });
+  test.beforeEach(async ({ page }) => {
+    await homepage.openHomePage(page);
+    await cookies.acceptCookiesIfVisible(page);
+    await withdrawPage.goToDeposit(page);
+
+  });
+    test("1 Payment method Interac", async ({ page }) => {
+    
+    await withdrawPage.interacPayment(page);
+  });
+    test("2 Payment method Visa", async ({ page }) => {
+    await withdrawPage.visaPayment(page);
+  });
+     test("3 Payment method Paysafecard", async ({ page }) => {
+      await withdrawPage.paysafecardPayment(page);
+  });
+     test("4 Payment method CashToCode EVoucher", async ({ page }) => {
+    await withdrawPage.cashToCodeEVoucherPayment(page);
+  });
+     test("5 Payment method JetonCash", async ({ page }) => {
+      await withdrawPage.jetonCashPayment(page);
+  });
+     test("6 Payment method LuxonPay", async ({ page }) => {
+      await withdrawPage.luxonPayPayment(page);
+  });
+     test("7 Payment method Mastercard", async ({ page }) => {
+      await withdrawPage.mastercardPayment(page);
+  });
+     test("8 Payment method MiFinity", async ({ page }) => {
+      await withdrawPage.miFinityPayment(page);
+  });
+     test("9 Payment method MuchBetter", async ({ page }) => {
+      await withdrawPage.muchBetterPayment(page);
+  });
+     test("10 Payment method Neosurf", async ({ page }) => {
+      await withdrawPage.neosurfPayment(page);
+  });
+     test("11 Change payment method", async ({ page }) => {
+      await withdrawPage.interacPayment(page);
+      await withdrawPage.changePaymentMethod(page);
+  });
+
 });
